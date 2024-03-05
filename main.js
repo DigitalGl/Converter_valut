8f437d2beb48819f836321d11cb3ca2ab04d5bb8
Daniil <daniil424342@gmail.com>
2024-03-05 19:24:24 +0500

    new for

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
-for (let i = 0; i < 4; i++) {
-    console.log(1);
+const input = prompt('Введите число')
+const number = Number(input)
+
+
+if (!isNaN(number) && number >= 0 && number <= 10) {
+    for (let i = 1; i <= 10; i++) {
+        console.log(`${number} * ${i} = ${i*number}`)
+    }
 }
