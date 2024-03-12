ee1c61682771289819b854de6edd469d1e419e35
Daniil <daniil424342@gmail.com>
2024-03-12 18:51:46 +0500

    ree

--- zam.js
+++ zam.js
@@ -15,3 +15,47 @@
 // const helloWorld = createGreeting();
 
 // helloWorld("word")
+
+
+
+// function addByx(num) {
+//     return function(x) {
+//         return num + x;
+//     }
+// }
+
+// const addByTwo = addByx(2);
+// const addByFive = addByx(5);
+
+// console.log(addByTwo(3)) // 5
+// console.log(addByTwo(6)) // 8
+
+// console.log(addByFive(6)) // 11
+// console.log(addByFive(2)) // 7
+
+
+
+
+
+
+function once(fn) {
+    let  hasBeenValled = false 
+    let cachedResult;
+
+    return function(...args) {
+        if (hasBeenValled) return cachedResult;
+
+        hasBeenValled = true;
+        cachedResult = fn(...args);
+
+        return cachedResult;
+    }
+}
+
+const addByTwoOnce = once(num => num + 2);
+
+console.log(addByTwoOnce(2));
+console.log(addByTwoOnce(5));
+console.log(addByTwoOnce(7));
+
+
