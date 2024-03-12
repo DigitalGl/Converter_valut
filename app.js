07d45236b6b19761641b38061c656c21551d2405
Daniil <daniil424342@gmail.com>
2024-03-12 22:14:50 +0500

    gdd

--- app.js
+++ app.js
@@ -1,16 +1,82 @@
 
 
 
-function createCalcFunction(n) {
-    return function() {
-        console.log(1000 * n);
+// function createCalcFunction(n) {
+//     return function() {
+//         console.log(1000 * n);
+//     }
+// }
+
+
+// const calc = createCalcFunction(42)
+
+// calc()
+
+
+
+
+
+
+
+
+
+
+
+
+// function createIncrementor(n) {
+//     return function(num) {
+//         return n + num;
+//     }
+// }
+
+// const addOne = createIncrementor(1)
+// const addTen = createIncrementor(10)
+
+// console.log(addOne(10));
+// console.log(addOne(41));
+
+// console.log(addTen(10));
+// console.log(addTen(41));
+
+
+
+
+
+
+
+// function urlGenerator(domain) {
+//     return function(url) {
+//         return `https://${url}.${domain}`
+//     }
+// }
+
+// const comUrl = urlGenerator('com')
+// const ruUrl = urlGenerator('ru')
+
+// console.log(comUrl('google'));
+// console.log(comUrl('youtube'));
+
+// console.log(ruUrl('youtube'));
+// console.log(ruUrl('vkontakte'));
+
+
+
+
+
+function bind(context, fn) {
+    return function(...args) {
+        fn.apply(context, args)
     }
 }
 
 
-createCalcFunction(42)
+function logPerson() {
+    console.log(`Person: ${this.name}, ${this.age}, ${this.job}`);
+}
+
+const person1 = {name: '', age: 22, job: 'Frontend'}
+const person2 = {name: '', age: 19, job: 'SMM'}
 
 
-
-
-
+bind(person1, logPerson)()
+bind(person2, logPerson)()
