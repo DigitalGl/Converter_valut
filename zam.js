1d688119f26472640a2a0173069b8e8fd7f014f1
Daniil <daniil424342@gmail.com>
2024-03-12 17:19:26 +0500

    fff

---
+++ zam.js
@@ -0,0 +1,17 @@
+
+// Фнукция которая генерирует другие функции
+// Функция - конструктор других функций
+
+// function createGreeting() {
+//     const message = 'hello';
+
+//     function greeting(str) {
+//         console.log(message, str);
+//     }
+
+//     return greeting
+// }
+
+// const helloWorld = createGreeting();
+
+// helloWorld("word")
