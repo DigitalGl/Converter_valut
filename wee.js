4b3df8c9f3ceaeb082d810b965a2b4860d01e1be
Daniil <daniil424342@gmail.com>
2024-03-21 16:45:25 +0500

    delet

--- wee.js
+++ wee.js
@@ -1,29 +1,6 @@
 
 
-let arr = [
-    {
-        name: 'Оля',
-        age: 22
-    },
-    {
-        name: 'Павел',
-        age: 32
-    },
-    {
-        name: 'Алекс',
-        age: 19
-    },
-    {
-        name: 'Борис',
-        age: 15
-    },
-]
 
-const sortUsers = (arr, prop, dir = false) => arr.sort((a,b) => !dir ? a[prop] < b[prop] : a[prop] > b[prop] ? -1 : 1);
-
-
-
-console.log(sortUsers(arr, 'age', true));
 
 
 
