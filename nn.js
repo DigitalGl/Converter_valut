85a7e0c305466acf3599a84b887b4dc0e341fb31
Daniil <daniil424342@gmail.com>
2024-03-23 11:28:05 +0500

    ffjs

---
+++ nn.js
@@ -0,0 +1,17 @@
+
+
+
+for (let i = 0; i < 6; i++) {
+    console.log(i);
+}
+
+while ( k < 5 ) {
+
+}
+
+
+
+
+
+
+
---
+++ test.js
@@ -0,0 +1,84 @@
+
+// Задача что-бы сделать квадрат из звездочек в циклах
+// **********
+// *        *
+// *        *
+// *        *
+// **********
+
+// let horizCount = 10;
+// let vertCount = 5;
+// let star = '*';
+// let whilespase = ' ';
+
+// for (let i = 0; i < vertCount; i++) {
+//     let str = '';
+
+//     if (i == 0 || i == vertCount - 1) {
+//         for (let j = 0; j < horizCount; ++j) {
+//             str += star;
+//         }
+//     } else {
+//         str = star;
+//         for (let j = 0; j < horizCount - 2; ++j) {
+//             str += whilespase;
+//         }
+//         str += star;
+//     }
+
+//     console.log(str);
+// }
+
+
+
+
+
+
+
+
+
+
+// Задача на циклы нарисовать триугольник 
+
+
+// let lines = 7;
+// let str = ' ';
+// let star = '*';
+// for (let i = 0; i < lines; i++) {
+//     str += star;
+//     console.log(str);
+// }
+
+
+
+
+
+// let i = 0;
+// j = 0;
+
+// let max = 15;
+// let spase = '';
+// star = '';
+
+// while (i < max) {
+//     spase = '';
+//     star = '';
+//     for (j = 0; j < max - i; j++) spase += " ";
+//     for (j = 0; j < 2 * i + 1; j++) star += "*";
+//     console.log(spase + star);
+//     i++
+// }
+
+
+
+
+let wordd = "";
+for(let i = 1; i < 8 ; i ++) {
+wordd = wordd + "#"
+console.log(wordd)
+}
+
+
+
+
+
