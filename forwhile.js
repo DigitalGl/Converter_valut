e9b34aae05327cea9797776684630586ef7bc302
Daniil <daniil424342@gmail.com>
2024-03-26 16:32:57 +0500

    Frilanser po jizni

---
+++ forwhile.js
@@ -0,0 +1,96 @@
+
+
+// let num = 5;
+// while (num) {
+//     console.log(num);
+//     num--;
+// }
+
+
+
+// let num = 0;
+// do {
+//     console.log(num);
+//     num++;
+// } while (num < 0);
+
+
+
+
+
+
+
+// Пример 
+
+// for (let num = 0; num < 5; num++) {
+//     console.log(num);
+// }
+
+/* 
+Работа цикла for;
+1)Выполняется начало - let num = 0
+2)Выполняется условие - num < 5
+3)Если условие true выполняется 
+    тело цикла - console.log(num)
+4)Выполняется шаг - num++
+Повтор начинается с пункта №2
+*/
+
+
+// Метки
+// firstFor: for ( let num = 0; num < 2; num++) {
+//     for (let size = 0; size < 3; size++) {
+//         if (size == 2) {
+//             break firstFor;
+//         }
+//         console.log(size);
+//     }
+// }
+
+
+
+// 1
+// for (let i = 1; i <= 5; i++) {
+//     console.log(i);
+// }
+
+
+
+
+//2
+// let num1 = 8;
+// while (num1) {
+//     //Последний результат 0, верно? (нет)
+//     console.log(num1);
+//     num1--;
+// }
+
+
+
+
+//3 Переписать на while
+// for (let num = 0; num < 3; num++) {
+//     console.log(`Число: ${num}`);
+// }
+
+// let num = 0;
+// while ( num < 3) {
+//     console.log(`Число: ${num}`);
+//     num++;
+// }
+
+
+//4 Прекращать работу цикла
+
+    //Цикл №1
+// firstFor: for (let num = 0; num, 2; num++) {
+//     //Цикл №2
+//     for (let size = 0; size < 3; size++) {
+//         if (size == 1) {
+//             break firstFor;
+//         }
+//         console.log(size);
+//     }
+// }
+
+
--- test.js
+++ test.js
@@ -72,15 +72,26 @@
 
 
 
-for (let i = 0; i < 6; i++) {
-    console.log(i);
-}
+// for (let i = 0; i < 6; i++) {
+//     console.log(i);
+// }
 
 
-let k = 0;
-while (k < 5) {
-    console.log('k: ' + k);
-    k++;
-}
+// let k = 0;
+// while (k < 5) {
+//     k++;
+//     console.log('k: ' + k);
+    
+// }
+
+
+// let sum = 0;
+// let p = 0;
+// while (p <= 3) {
+//     sum = sum + p;
+//     p++
+// }
+
+// console.log('sum = ' + sum);
 
 
