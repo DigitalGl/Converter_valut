877faacdc852a7d1c82175bea93a2799ec80fa82
Daniil <daniil424342@gmail.com>
2024-03-29 21:53:28 +0500

    fff

---
+++ object.js
@@ -0,0 +1,19 @@
+
+
+//Синтаксис
+
+//Создание обьектов
+
+// let userInfo = new Object(); //Синтаксис "конструктор обьекта"
+// let userInfo1 = {}; //синтаксис "литерал обьекта"
+
+
+let userInfo = {
+    name: "Глеб", //Свойство обьекта
+    age: 30, //Последняя (висячая) запятая
+};
+
+console.log(userInfo);
+console.log(userInfo.name);
+
+console.log(userInfo.name);
