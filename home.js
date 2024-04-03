ca89a582c10ced15e66d76ec2a1847a8150d195f
Daniil <daniil424342@gmail.com>
2024-04-03 12:08:24 +0500

    home

---
+++ home.js
@@ -0,0 +1,23 @@
+
+
+let userInfo = {}
+
+userInfo.name = "Вася";
+console.log(userInfo);
+
+userInfo.age = 30;
+console.log(userInfo);
+
+
+userInfo.name = "Лена";
+console.log(userInfo);
+
+
+delete userInfo.name
+console.log(userInfo);
+
+
+
+
+
+
