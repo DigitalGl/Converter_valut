f9ca42fd314ed4611c31df052e38da3ca93025bd
Daniil <daniil424342@gmail.com>
2024-06-10 13:09:20 +0500

    two commit

--- vain.js
+++ vain.js
@@ -1,1 +1,19 @@
-console.log("Hello world")
+
+
+
+function sayHi(name) {
+    console.log(`Привет, ${name}! Как твои дела?`);
+}
+
+
+
+sayHi("Mark");
+
+
+
+const sayHi2 = (name) => {
+    console.log(`Привет, ${name}! Как твои дела?`);
+} 
+
+
+sayHi2("Mark");
