4ee7597b9a2472c3d9ee83e53509e95aeddde452
Daniil <daniil424342@gmail.com>
2024-06-19 14:19:01 +0500

    dom tests

--- vain.js
+++ vain.js
@@ -5,3 +5,11 @@
 document.getElementsByClassName(); // Выбор колллекции элементов по CSS классу
 document.getElementsByTagName(); // Выбор коллекции элементов по тегу
 document.getElementsById(); // Выбор одного элемента по ID
+
+
+
+
+
+const button = document.querySelector('#button');
+button.setAttribute('value', 'Отправить');
+button.value = "Текст для кнопки";
