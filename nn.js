8ceb75ccd572d894b08b488a4eaebf59a725f0f1
Daniil <daniil424342@gmail.com>
2024-06-21 23:31:21 +0500

    ff

--- nn.js
+++ nn.js
@@ -1,34 +1,36 @@
+const { log } = require("console");
 
 
 myPromise.then(function (data) {
 
-    return new Promise(function(resolve, reject){
-        setTimeout(() => {
-            console.log('Then 1');
-            console.log(data);
+	return new Promise(function (resolve, reject) {
+		setTimeout(() => {
+			console.log('Then 1');
+			console.log(data);
 
-            const response = false;
-            if (response) {
-                resolve('Data from then 1')
-            } else {
-                reject('Data from then 1')
-            }
-        }, 1000)
-    })
-}).then(function(data) {
+			const response = false;
+			if (response) {
+				resolve('Data from then 1')
+			} else {
+				reject('Data from then 1')
+			}
+		}, 1000)
+	})
+}).then(function (data) {
 
-    setTimeout(() => {
-        console.log('Then 2');
-        console.log(data);
-    })
+	setTimeout(() => {
+		console.log('Then 2');
+		console.log(data);
+	})
 
 }).catch(function (data) {
-    console.log('Catch');
-    console.log(data);
+	console.log('Catch');
+	console.log(data);
 })
 
-
-
+if (a === b) {
+	console.log();
+}
 
 
 
